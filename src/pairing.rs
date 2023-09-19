ab1d285d7f2325b255f7d006d1f96fb4c5e5f0ae
mariobenissimo <mariobenissimo@gmail.com>
2023-09-19 17:50:03 +0200

    try to add test

--- src/pairing.rs
+++ src/pairing.rs
@@ -1,35 +1,118 @@
-use ark_ec::pairing::Pairing;
-use ark_r1cs_std::fields::fp::{FpVar};
-use ark_r1cs_std::{prelude::*};
+use ark_ec::{pairing::{Pairing, PairingOutput}, CurveGroup};
+
+use ark_r1cs_std::prelude::*;
 use ark_relations::{
     ns,
-    r1cs::{ConstraintSynthesizer, ConstraintSystem, ConstraintSystemRef, SynthesisError},
-  };
-use ark_std::marker::PhantomData;
+    r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError},
+};
+
+use ark_std::{
+    marker::PhantomData,
+    rand::{CryptoRng, Rng},
+    UniformRand,
+};
+use ark_relations::r1cs::Namespace;
+use std::borrow::Borrow;
 
 #[derive(Clone)]
-struct KeyVerification<I,IV>
+struct KeyVerification<I, IV>
 where
-I: Pairing,
-IV: PairingVar<I>,
+    I: Pairing,
+    IV: PairingVar<I>,
 {
-    x: I::ScalarField,
-    y: I::G1,
+    x: I::G1,
+    y: I::G2,
+    z: PairingOutput<I>,  //Fqk
     _iv: PhantomData<IV>,
     _i: PhantomData<I>,
 }
 
-impl<I,IV> ConstraintSynthesizer<I::ScalarField> for KeyVerification<I,IV>
+impl<I, IV> KeyVerification<I, IV>
 where
-I: Pairing,
-IV: PairingVar<I>,
+    I: Pairing,
+    IV: PairingVar<I>,
+    IV::G1Var: CurveVar<I::G1, I::BaseField>,
+    IV::G2Var: CurveVar<I::G2, I::BaseField>,
+    Namespace<<<I as Pairing>::G1 as CurveGroup>::BaseField>: From<ConstraintSystemRef<<I as Pairing>::BaseField>>, //here
+    PairingOutput<I>: Borrow<<I as Pairing>::TargetField>
+    
+    {
+    #[allow(dead_code)]
+    pub fn new<R: Rng + CryptoRng>(
+        mut rng: &mut R,
+    ) -> Self {
+        let x = I::G1::rand(&mut rng);
+        let y = I::G2::rand(&mut rng);
+        let x_prep = I::G1Prepared::from(x);
+        let y_prep = I::G2Prepared::from(y);
+        let z = I::pairing(x_prep, y_prep);
+
+        Self {
+            x: x,
+            y: y,
+            z: z,
+            _iv: PhantomData,
+            _i: PhantomData,
+        }
+
+
+    }
+
+}
+impl<I, IV> ConstraintSynthesizer<<I as Pairing>::BaseField> for KeyVerification<I, IV>
+where
+    I: Pairing,
+    IV: PairingVar<I>,
+    IV::G1Var: CurveVar<I::G1, I::BaseField>,
+    IV::G2Var: CurveVar<I::G2, I::BaseField>,
+    Namespace<<<I as Pairing>::G1 as CurveGroup>::BaseField>: From<ConstraintSystemRef<<I as Pairing>::BaseField>>, //here
+    PairingOutput<I>: Borrow<<I as Pairing>::TargetField>
+
 {
-    fn generate_constraints(self, cs: ConstraintSystemRef<I::ScalarField>) -> Result<(), SynthesisError> {
-        let a: FpVar<<I as Pairing>::ScalarField> = FpVar::new_witness(cs, || Ok(self.x))?;
-        //ERROR HERE
-        //the trait bound `<IV as ark_r1cs_std::pairing::PairingVar<I>>::G1Var: ark_r1cs_std::alloc::AllocVar<_, <I as Pairing>::ScalarField>` is not satisfied
-        let b = IV::G1Var::new_witness(cs, || Ok(self.y))?;
+    fn generate_constraints(
+        self,
+        cs: ConstraintSystemRef<<I as Pairing>::BaseField>,
+    ) -> Result<(), SynthesisError> {
+       
+        let xvar = IV::G1Var::new_input(cs.clone(), || Ok(self.x))?;
+      
+        let yvar = IV::G2Var::new_input(cs.clone(), || Ok(self.y))?;
+
+        let exp_res = IV::GTVar::new_input(cs.clone(), || Ok(self.z))?; // here
+
+        let x_prepared = IV::prepare_g1(&xvar)?;
+        let y_prepared = IV::prepare_g2(&yvar)?;
+
+        let pair = IV::pairing(x_prepared, y_prepared)?;
+
+        exp_res.enforce_equal(&pair)?;
 
         Ok(())
     }
-  }
+}
+
+#[cfg(test)]
+mod tests {
+
+/// Specifies the constraints for computing a pairing in a BLS12 bilinear group
+    use crate::pairing::Pairing;
+    use ark_bls12_377::{
+      constraints::PairingVar as IV, Bls12_377 as I,
+      constraints::*,
+  };
+
+    use ark_ec::bls12::Bls12;
+    use ark_relations::r1cs::ConstraintSystem;
+    use super::*;
+    #[test]
+    fn preimage_constraints_correctness() {
+        let mut rng = ark_std::test_rng();
+        let cs = ConstraintSystem::<<Bls12<ark_bls12_377::Config> as Pairing>::BaseField>::new_ref();
+
+        KeyVerification::<I, IV>::new(&mut rng).generate_constraints(cs.clone()).unwrap();
+        
+        assert!(cs.is_satisfied().unwrap());
+
+    }
+
+    }
